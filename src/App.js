9566077e792bba907cb38410ecd759c7ebd9bf64
Senad Djerlek <senajerlek@gmail.com>
2022-09-07 23:34:29 +0200

    added buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Tabs align="center" variant="soft-rounded" colorScheme="purple">
+        <TabList>
+          <Tab>Iphone</Tab>
+          <Tab>Samsung</Tab>
+          <Tab>Tesla</Tab>
+          <Tab>New York</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <p>one!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>three!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>New York</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </ChakraProvider>
   );
 }
 
